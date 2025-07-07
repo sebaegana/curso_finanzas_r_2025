880608740a8c60f604f187717f80cc4548d34fb4
sebagana <sebaegana@gmail.com>
2025-07-07 18:39:16 -0400

    Initial commit

---
+++ clase_01.R
@@ -0,0 +1,11 @@
+x = 2
+y = 5
+xy = 'Hey'
+
+# Podemos correr el código con control + enter
+
+print(paste(x,y,xy))
+
+print(paste(x,y,xy, sep = "+"))
+
+df <- tibble(x_1 = c(1, 2), x_2 = c(3, 4), y = c(4, 5))
